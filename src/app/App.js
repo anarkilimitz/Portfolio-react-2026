18377e7741cf866c321da25f7db048cbcec82b38
Evgenii Borisovich <anarkilimitz@gmail.com>
2025-10-10 23:57:07 +0500

    first commit

---
+++ src/app/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles/global.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+	return (
+		<div>
+			<a
+				className="App-link"
+				href="https://reactjs.org"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Learn React
+			</a>
+		</div>
+	);
+}
+
+export default App;
