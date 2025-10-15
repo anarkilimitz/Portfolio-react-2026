426d392c48bb87a2f95840f430860cd551ff933e
Evgenii Borisovich <anarkilimitz@gmail.com>
2025-10-15 18:04:18 +0500

    finish-form

--- src/shared/ui/forms/form.js
+++ src/shared/ui/forms/form.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from '../forms/form.module.scss';
-
 import { Formik, Form, useField } from 'formik';
 import * as Yup from 'yup';
 
@@ -9,13 +8,14 @@ const MyTextInput = ({ label, as, ...props }) => {
 	const Component = as || 'input';
 	return (
 		<>
-			<label htmlFor={props.name}>{label}</label>
+			<label htmlFor={props.name} className={styles.label}>
+				{label}
+			</label>
 			<Component
 				{...props}
 				{...field}
-				className={styles[props.className] || ''}
+				className={styles[props.className] || styles.input}
 			/>
-
 			{meta.touched && meta.error ? (
 				<div className={styles.error}>{meta.error}</div>
 			) : null}
@@ -28,10 +28,14 @@ const MyCheckbox = ({ children, ...props }) => {
 	return (
 		<>
 			<label className={styles.checkbox}>
-				<input type="checkbox" {...props} {...field} />
+				<input
+					type="checkbox"
+					{...props}
+					{...field}
+					className={styles.checkboxInput}
+				/>
 				{children}
 			</label>
-
 			{meta.touched && meta.error ? (
 				<div className={styles.error}>{meta.error}</div>
 			) : null}
@@ -52,16 +56,13 @@ const MainForm = () => {
 				name: Yup.string()
 					.min(2, 'Введите не менее 2 символов')
 					.required('Обязательно для заполнения'),
-
 				email: Yup.string()
 					.email('Некорректно введен email')
 					.matches(/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/, 'Введите корректный email')
 					.required('Обязательно для заполнения'),
-
 				text: Yup.string()
 					.min(10, 'Введите не менее 10 символов')
 					.required('Обязательно для заполнения'),
-
 				terms: Yup.boolean()
 					.required('Необходимо согласие!')
 					.oneOf([true], 'Необходимо согласие!'),
@@ -69,17 +70,14 @@ const MainForm = () => {
 			onSubmit={(values) => {}} // для отправки формы на БУДУЩЕЕ
 		>
 			<Form className={styles.form}>
-				<h2 className="form__text-header">Можете написать мне</h2>
-
+				<h2 className={styles.formHeader}>Отправить сообщение</h2>
 				<MyTextInput id="name" name="name" type="text" placeholder="Ваше имя" />
-
 				<MyTextInput
 					id="email"
 					name="email"
 					type="email"
 					placeholder="Ваша почта"
 				/>
-
 				<MyTextInput
 					id="text"
 					name="text"
@@ -87,12 +85,14 @@ const MainForm = () => {
 					className="textarea"
 					placeholder="Ваше сообщение"
 				/>
-
 				<MyCheckbox name="terms">
-					<a href="#">Соглашаетесь с политикой конфиденциальности?</a>
+					<a href="#" className={styles.link}>
+						Соглашаетесь с политикой конфиденциальности?
+					</a>
 				</MyCheckbox>
-
-				<button type="submit">Отправить</button>
+				<button type="submit" className={styles.button}>
+					Отправить
+				</button>
 			</Form>
 		</Formik>
 	);
