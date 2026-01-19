fdc333f539f25114e7dcc3899c4933803b837036
Evgenii Borisovich <anarkilimitz@gmail.com>
2026-01-19 23:53:59 +0500

    add-carousel-header

--- src/widgets/header/header.js
+++ src/widgets/header/header.js
@@ -4,6 +4,21 @@ import styles from './header.module.scss';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 
 function Header() {
+	const technologies = [
+		'HTML5',
+		'CSS3',
+		'JavaScript',
+		'React',
+		'SASS/SCSS',
+		'Bootstrap',
+		'Git',
+		'REST API',
+		'Webpack',
+		'Figma',
+		'Responsive Design',
+		'UI/UX',
+	];
+
 	return (
 		<>
 			<Navbar
@@ -27,13 +42,44 @@ function Header() {
 			<header
 				className={`min-vh-100 d-flex align-items-stretch ${styles.header}`}
 			>
-				<Container fluid className="p-0 h-100">
+				{/* title */}
+				<Container fluid className="p-5 h-100">
 					<div className="text-white p-5" style={{ marginTop: '280px' }}>
-						<h1>Hi, I'm Evgeniy</h1>
-						<h3>Front-End JavaScript & React Developer</h3>
+						<h1 className="pb-3">Hi, I'm Evgeniy</h1>
+						<h3 className="pb-3">Front-End JavaScript & React Developer</h3>
 						<p>I build modern, responsive web applications.</p>
 					</div>
 				</Container>
+
+				{/* бегущая строка */}
+				<div className={styles.fullWidthTechnologies}>
+					<Container fluid className="px-0">
+						<div className={styles.technologiesContainer}>
+							<div className={styles.technologiesWrapper}>
+								<div className={styles.technologiesTrack}>
+									{/* первый набор */}
+									{technologies.map((tech, index) => (
+										<span key={`first-${index}`} className={styles.techItem}>
+											{tech}
+										</span>
+									))}
+									{/* второй набор */}
+									{technologies.map((tech, index) => (
+										<span key={`second-${index}`} className={styles.techItem}>
+											{tech}
+										</span>
+									))}
+									{/* третий набор */}
+									{technologies.map((tech, index) => (
+										<span key={`third-${index}`} className={styles.techItem}>
+											{tech}
+										</span>
+									))}
+								</div>
+							</div>
+						</div>
+					</Container>
+				</div>
 			</header>
 		</>
 	);
