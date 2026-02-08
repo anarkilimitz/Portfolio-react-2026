47830d44988a8324cc049f068b02dcd45651e09f
Evgenii Borisovich <anarkilimitz@gmail.com>
2026-02-08 21:00:20 +0500

    change-styles-SeitcherSlider

--- src/shared/ui/buttons/switcherSlider/switcherSlider.js
+++ src/shared/ui/buttons/switcherSlider/switcherSlider.js
@@ -1,35 +1,26 @@
 import styles from './switcherSlider.module.scss';
 
-import { Button, ButtonGroup } from 'react-bootstrap';
-// import {useSlider} from
-
-function SwitcherSlider() {
-	// const { slide, switchSlide } = useSlider();
-
+function SwitcherSlider({ slide, onSwitch }) {
 	return (
-		<div className={styles.sliderSwitcher}>
-			<ButtonGroup>
-				<Button
-					variant="none"
-					// className={`${styles.sliderSwitcherBtn} ${
-					// 	slide === 'IMAGE' ? styles.active : ''
-					// }`}
-					// onClick={() => switchSlide('IMAGE')}
-				>
-					IMAGE
-				</Button>
-				<Button
-					variant="none"
-					// className={`${styles.sliderSwitcherBtn} ${
-					// 	slide === 'DETAILS' ? styles.active : ''
-					// }`}
-					// onClick={() => switchSlide('DETAILS')}
-				>
-					DETAILS
-				</Button>
-			</ButtonGroup>
+		<div className={styles.switcherSlider}>
+			<button
+				className={`${styles.button} ${slide === 'IMAGE' ? styles.active : ''}`}
+				onClick={() => onSwitch('IMAGE')}
+			>
+				IMAGE
+			</button>
+
+			<button
+				className={`${styles.button} ${
+					slide === 'DETAILS' ? styles.active : ''
+				}`}
+				onClick={() => onSwitch('DETAILS')}
+			>
+				DETAILS
+			</button>
 		</div>
 	);
 }
 
 export default SwitcherSlider;
+
